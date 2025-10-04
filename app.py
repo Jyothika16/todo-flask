1d6149fabaf0ef5b32acacd99f91b64361a1ff7a
Jyothika Meda <jyothikameda16.com>
2025-10-04 09:40:09 +0530

    cleaned requirements.txt for Railway deployment

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 tasks = []
+
 @app.route('/')
 def index():
     return render_template('index.html', tasks=tasks)
@@ -11,7 +13,6 @@ def add():
     task = request.form.get('task')
     if task:
         tasks.append({"content": task, "status": "Pending"})
-
     return redirect(url_for('index'))
 
 @app.route('/done/<int:task_id>')
@@ -23,30 +24,21 @@ def done(task_id):
 @app.route('/view')
 def view():
     return render_template('view.html', tasks=tasks)
-# Route to update a task
+
 @app.route('/update/<int:task_id>', methods=['GET', 'POST'])
 def update(task_id):
     if request.method == 'POST':
         new_task = request.form['task']
-        tasks[task_id] = new_task  # replace old task with new one
+        tasks[task_id]['content'] = new_task  # update task content
         return redirect(url_for('index'))
     return render_template('update.html', task=tasks[task_id], task_id=task_id)
-
-
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
     if 0 <= task_id < len(tasks):
         tasks.pop(task_id)
     return redirect(url_for('index'))
-@app.route('/exit')
-def exit_app():
-    os._exit(0)  # stops Flask server
-    return "Exiting..."
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
